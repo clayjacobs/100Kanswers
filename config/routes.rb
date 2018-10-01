b03d2dcfed0bb802a625a77ea054f63cd2715c5f
EC2 Default User <ec2-user@ip-172-31-27-229.us-east-2.compute.internal>
2018-10-01 09:18:01 +0000

    new people

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'user#new'
+  resources :people
+  resources :users
+  get '/signup', to: 'person#new'
 
   get '/about', to: 'static_pages#help'
 
