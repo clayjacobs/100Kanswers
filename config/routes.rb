06ec3b13faba38cdc867107171b861695e8a6c9e
EC2 Default User <ec2-user@ip-172-31-27-229.us-east-2.compute.internal>
2018-10-02 03:55:47 +0000

    New form Gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :people
   resources :users
-  get '/signup', to: 'person#new'
+  get '/signup', to: 'people#new'
 
   get '/about', to: 'static_pages#help'
 
